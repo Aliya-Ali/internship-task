910fdb2a7b0de56031f39f455702a20aae0e9666
Aliya-Ali <aliyaali33804@gmail.com>
2025-07-08 18:09:42 +0500

    ten commit

--- business-bot/app.py
+++ business-bot/app.py
@@ -49,8 +49,8 @@ def index():
                 profile_url = post_url  # to display under button
                 
         except Exception as e:
-            print("❌ Error:", e)
-            message = f"❌ Error: {str(e)}"
+            # print("❌ Error:", e)
+            message = f"Error: {e}"
 
     return render_template("index.html", message=message, profile_url=profile_url, last_action=action)
 
